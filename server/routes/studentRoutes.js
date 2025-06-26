509c69980f3a849b2cf9c82dbf1bcc82dd1f02f5
i_monibKS <singhamonib1327@gmail.com>
2025-06-26 13:34:02 +0530

    Update CRUD in ManageStudents

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -29,4 +29,49 @@ router.post('/', async(req, res) => {
     }
 })
 
+// student list
+router.get('/', async (req, res) => {
+    try {
+        const students = await Student.find()
+        res.json(students)
+    }
+    catch (err) {
+        res.status(500).json({error: 'Server error'})
+    }
+})
+
+// delete a student by ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedStudent = await Student.findByIdAndDelete(req.params.id);
+        if (!deletedStudent) {
+            return res.status(404).json({error: 'Student not found'})
+        }
+        res.json({message: 'Student deleted successfully'})
+    }
+    catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+// updata a student by id
+router.put('/:id', async (req, res) => {
+    const {username, password, roomNumber} = req.body;
+
+    try {
+        const student = await Student.findByIdAndUpdate(
+            req.params.id,
+            {username, password, roomNumber},
+            {new: true}
+        )
+        if(!student){
+            return res.status(404).json({error: 'Student not found'});
+        }
+        res.json({message: 'Student updated successfully', student})
+    }
+    catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
 module.exports = router
